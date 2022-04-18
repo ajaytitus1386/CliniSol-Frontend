28d9fbe6a4419fff1d394a3db05a1040b9bb7e49
Ajay Titus <ajaytitus1386@gmail.com>
2022-04-18 21:57:34 +0530

    feat: import bulma custom css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
+// Import Custom Modified Bulma Sass Compiled CSS
+import "../css/custom.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>CliniSol</title>
+        {/* Imports the CSS which is compiled from Sass By Bulma */}
+        {/* <link rel="stylesheet" href="../styles/globals.css"></link> */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
