77e8554f61d2553a3cae24f7de6253fb94d0003f
Ajay Titus <ajaytitus1386@gmail.com>
2022-04-22 12:28:56 +0530

    feat: add MUI icons and layout wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@
 import "../css/custom.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Layout from "../components/global/Layout";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
@@ -9,8 +10,16 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>CliniSol</title>
         {/* Imports the CSS which is compiled from Sass By Bulma */}
         {/* <link rel="stylesheet" href="../styles/globals.css"></link> */}
+
+        {/* Material UI Icons */}
+        <link
+          href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp"
+          rel="stylesheet"
+        />
       </Head>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   );
 }
